986fd19b0c5c27c17b7daa4f2954c3ad7b929728
NguyenHuuQuangMinh <21T1020516@husc.edu.vn>
2025-02-14 16:12:14 +0700

    hhh

--- TEAM_01/Nguyen_Huu_Quang_Minh/ESP32_LED_Blink/src/main.cpp
+++ TEAM_01/Nguyen_Huu_Quang_Minh/ESP32_LED_Blink/src/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
 
-int ledPin = 5;
-bool IsLed_On = false;
-ulong led_start = 0;
+int ledPinred = 5;
+int ledPingreen = 4;
+int ledPinyellow = 16;
+
+// Thời gian sáng của mỗi đèn
+const uint32_t RED_TIME = 10000;
+const uint32_t YELLOW_TIME = 3000;
+const uint32_t GREEN_TIME = 5000;
+
+// bool IsLed_On = false;
+// ulong led_start_red = 0;
+uint32_t previoustime = 0;
+int currentState = 0;
+
 // put function declarations here:
 int myFunction(int, int);
 
@@ -10,37 +21,72 @@ void setup() { // đoạn code khởi tạo
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  int result = myFunction(2, 3);
-  Serial.println("IOT welcome");
-  pinMode(ledPin, OUTPUT);
+  // int result = myFunction(2, 3);
+  // Serial.println("IOT welcome");
+  pinMode(ledPinred, OUTPUT);
+  pinMode(ledPingreen, OUTPUT);
+  pinMode(ledPinyellow, OUTPUT);
+
+  digitalWrite(ledPinred, HIGH);
+  digitalWrite(ledPingreen, LOW);
+  digitalWrite(ledPinyellow, LOW);
 }
 
 void Use_Blocking(){
-  digitalWrite(ledPin, HIGH); // Bật
+  digitalWrite(ledPinred, HIGH); // Bật
   Serial.println("LED -> ON");
   delay(1000);                // Chờ 1 giây có cơ chế block
-  digitalWrite(ledPin, LOW);  // tắt
+  digitalWrite(ledPinred, LOW);  // tắt
   Serial.println("LED -> OFF");
   delay(1000);
 }
 
-bool iSReady(ulong& ulTimer, uint32_t milisecond){
-  ulong t = millis();
+bool iSReady(uint32_t& ulTimer, uint32_t milisecond){
+  uint32_t t = millis();
   if(t - ulTimer < milisecond) return false;
   ulTimer = t;
   return true;
 }
 
 void Use_Non_Blocking(){
-  if(!iSReady(led_start,1000)) return;
-  if(!IsLed_On){
-    digitalWrite(ledPin, HIGH); // Bật
-    Serial.println("NonBlocking LED -> ON");
-  }else{
-    digitalWrite(ledPin, LOW); // Bật
-    Serial.println("NonBlocking LED -> OFF");
+  // if(!iSReady(led_start_red,5000)) return;
+  // if(!IsLed_On){
+  //   digitalWrite(ledPinred, HIGH); // Bật
+  //   Serial.println("NonBlocking LED -> ON");
+  // }else{
+  //   digitalWrite(ledPinred, LOW); // Bật
+  //   Serial.println("NonBlocking LED -> OFF");
+  // }
+  // IsLed_On = !IsLed_On;
+  
+  switch (currentState)
+  {
+  case 0:
+    if(iSReady(previoustime, RED_TIME)){
+      digitalWrite(ledPinred, LOW);
+      digitalWrite(ledPinyellow, HIGH);
+      currentState = 1;
+      Serial.println("Đèn vàng sáng");
+    }
+    break;
+    case 1: // Đèn vàng
+    if (iSReady(previoustime, YELLOW_TIME)) {
+      digitalWrite(ledPinyellow, LOW);
+      digitalWrite(ledPingreen, HIGH);
+      currentState = 2;
+      Serial.println("Đèn xanh sáng");
+    }
+    break;
+  
+  case 2: // Đèn xanh
+    if (iSReady(previoustime, GREEN_TIME)) {
+      digitalWrite(ledPingreen, LOW);
+      digitalWrite(ledPinred, HIGH);
+      currentState = 0;
+      Serial.println("Đèn đỏ sáng");
+    }
+    break;
   }
-  IsLed_On = !IsLed_On;
 }
 
 void loop() { 
