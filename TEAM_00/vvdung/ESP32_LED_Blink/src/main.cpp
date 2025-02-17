97eb830805dcf306b5e8d06927d59d2b13beb79f
lanphan37 <phanngoclannc3@gmail.com>
2025-02-17 22:50:32 +0700

    .

--- TEAM_00/vvdung/ESP32_LED_Blink/src/main.cpp
+++ TEAM_00/vvdung/ESP32_LED_Blink/src/main.cpp
@@ -5,44 +5,52 @@ int ledPin = 5; // Chân GPIO 2 (thường nối sẵn LED trên board)
 bool isLED_ON = false;
 ulong ledStart = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
 }
 
-void Use_Blocking(){
+void Use_Blocking()
+{
   digitalWrite(ledPin, HIGH); // Bật LED
   Serial.println("LED -> ON");
-  delay(1000);                // Đợi 1 giây
-  digitalWrite(ledPin, LOW);  // Tắt LED
+  delay(1000);               // Đợi 1 giây
+  digitalWrite(ledPin, LOW); // Tắt LED
   Serial.println("LED -> OFF");
-  delay(1000);                // Đợi 1 giây  
+  delay(1000); // Đợi 1 giây
 }
 
-
-bool IsReady(ulong& ulTimer, uint32_t milisecond){
+bool IsReady(ulong &ulTimer, uint32_t milisecond)
+{
   ulong t = millis();
-  if (t - ulTimer < milisecond) return false;
+  if (t - ulTimer < milisecond)
+    return false;
   ulTimer = t;
   return true;
 }
 
-void Use_Non_Blocking(){
-  if (!IsReady(ledStart,1000)) return;
+void Use_Non_Blocking()
+{
+  if (!IsReady(ledStart, 1000))
+    return;
 
-  if (!isLED_ON){
+  if (!isLED_ON)
+  {
     digitalWrite(ledPin, HIGH); // Bật LED
-    Serial.println("NonBlocking LED -> ON");  
+    Serial.println("NonBlocking LED -> ON");
   }
-  else {
+  else
+  {
     digitalWrite(ledPin, LOW); // Tắt LED
-    Serial.println("NonBlocking LED -> OFF");  
+    Serial.println("NonBlocking LED -> OFF");
   }
   isLED_ON = !isLED_ON;
 }
 
-void loop() {
-  //Use_Blocking();
+void loop()
+{
+  // Use_Blocking();
   Use_Non_Blocking();
 
   ulong t = millis();
