e8ae7006fe2988d84cd19f71cd7b65259f2d91d8
Văn Huỳnh Tường An <21t1020212@husc.edu.vn>
2025-02-18 14:33:04 +0700

    .

--- TEAM_00/vhtan/Traffic light/src/main.cpp
+++ TEAM_00/vhtan/Traffic light/src/main.cpp
@@ -1,28 +1,83 @@
 #include <Arduino.h>
+#include <TM1637Display.h>
+//Pin
+#define rLED  5
+#define yLED  17
+#define gLED  16
 
-#define LED_RED 5      // Chân điều khiển đèn LED đỏ
-#define LED_YELLOW 0   // Chân điều khiển đèn LED vàng
-#define LED_GREEN 16   // Chân điều khiển đèn LED xanh
+//1000 ms = 1 seconds
+#define rTIME  5000   //5 seconds
+#define yTIME  2000
+#define gTIME  7000
+
+
+ulong currentMiliseconds = 0;
+ulong ledTimeStart = 0;
+ulong nextTimeTotal = 0;
+int currentLED = rLED;
+
+bool IsReady(ulong &ulTimer, uint32_t milisecond);
+void NonBlocking_Traffic_Light();
 
 void setup() {
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(rLED, OUTPUT);
+  pinMode(yLED, OUTPUT);
+  pinMode(gLED, OUTPUT);
+
+  
+  digitalWrite(yLED, LOW);
+  digitalWrite(gLED, LOW);
+  digitalWrite(rLED, HIGH);
+  currentLED = rLED;
+  nextTimeTotal += rTIME;
+  Serial.println("== START ==>");
+  Serial.print("1. RED \t\t => Next "); Serial.println(nextTimeTotal);
 }
 
 void loop() {
-  // Bật đèn xanh trong 5 giây
-  digitalWrite(LED_GREEN, HIGH);
-  delay(5000);
-  digitalWrite(LED_GREEN, LOW);
+  // put your main code here, to run repeatedly:
+  currentMiliseconds = millis();
+  NonBlocking_Traffic_Light();
+}
 
-  // Bật đèn vàng trong 2 giây
-  digitalWrite(LED_YELLOW, HIGH);
-  delay(2000);
-  digitalWrite(LED_YELLOW, LOW);
+bool IsReady(ulong &ulTimer, uint32_t milisecond)
+{
+  if (currentMiliseconds - ulTimer < milisecond) return false;
+  ulTimer = currentMiliseconds;
+  return true;
+}
+void NonBlocking_Traffic_Light(){
+  switch (currentLED) {
+    case rLED: // Đèn đỏ: 5 giây
+      if (IsReady(ledTimeStart, rTIME)) {
+        digitalWrite(rLED, LOW);
+        digitalWrite(gLED, HIGH);
+        currentLED = gLED;
+        nextTimeTotal += gTIME;
+        Serial.print("2. GREEN\t => Next "); Serial.println(nextTimeTotal);        
+      } 
+      break;
 
-  // Bật đèn đỏ trong 5 giây
-  digitalWrite(LED_RED, HIGH);
-  delay(5000);
-  digitalWrite(LED_RED, LOW);
+    case gLED: // Đèn xanh: 7 giây
+      if (IsReady(ledTimeStart,gTIME)) {        
+        digitalWrite(gLED, LOW);
+        digitalWrite(yLED, HIGH);
+        currentLED = yLED;
+        nextTimeTotal += yTIME;
+        Serial.print("3. YELLOW\t => Next "); Serial.println(nextTimeTotal);        
+      }
+      break;
+
+    case yLED: // Đèn vàng: 2 giây
+      if (IsReady(ledTimeStart,yTIME)) {        
+        digitalWrite(yLED, LOW);
+        digitalWrite(rLED, HIGH);
+        currentLED = rLED;
+        nextTimeTotal += rTIME;
+        Serial.print("1. RED \t\t => Next "); Serial.println(nextTimeTotal);        
+      }
+      break;
+  }
 }
