779c70e7900842c4da794a61b8ad25a320f04ef2
Tên mới <21T1020766@husc.edu.vn>
2025-02-18 14:12:00 +0700

    a

---
+++ TEAM_03/Trangle080/ESP32_LED_Blink/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+
+
+int ledPin = 5; // Chân GPIO 2 (thường nối sẵn LED trên board)
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Bật LED
+  Serial.println("LED -> ON");
+  delay(1000);                // Đợi 1 giây
+  digitalWrite(ledPin, LOW);  // Tắt LED
+  Serial.println("LED -> OFF");
+  delay(1000);                // Đợi 1 giây
+}
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
