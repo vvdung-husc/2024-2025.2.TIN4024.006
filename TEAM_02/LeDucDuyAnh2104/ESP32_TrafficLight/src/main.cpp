a2d4cb87ed9798bccebce56dc18bf43b0ee57a8c
LeDucDuyAnh2104 <21t1020225@husc.edu.vn>
2025-02-18 08:40:15 +0700

    baitaptraffic

--- TEAM_02/LeDucDuyAnh2104/ESP32_TrafficLight/src/main.cpp
+++ TEAM_02/LeDucDuyAnh2104/ESP32_TrafficLight/src/main.cpp
@@ -1,66 +1,60 @@
 #include <Arduino.h>
 
-const int redPin = 5;    
-const int yellowPin = 17; 
-const int greenPin = 16;  
+int redPin = 5;
+int yellowPin = 17;
+int greenPin = 16;
 
 unsigned long previousMillis = 0;
-const long interval = 1000;
+long interval = 1000;
 
-int countdown = 5;
+int countdown = 0;
 int state = 0;
-
-void setup() {
+void setup(){
   pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  
+
   Serial.begin(9600);
-
-
-   digitalWrite(greenPin, HIGH);
-   Serial.println("Đèn xanh (5 giây)");
-   countdown = 5;
-   state = 0;
+  digitalWrite(greenPin, HIGH);
+  Serial.println("Đèn xanh (5 giây)");
+  countdown = 5;
+  state = 0;
 }
-
-void changeLight() {
+void changeLight(){
   digitalWrite(redPin, LOW);
   digitalWrite(yellowPin, LOW);
   digitalWrite(greenPin, LOW);
 
-  if (state == 0) {
-    digitalWrite(yellowPin, HIGH);
-    Serial.println("Đèn vàng (2 giây)");
-    countdown = 2;
-    state = 1;
-  } else if (state == 1) { 
+  if(state == 0){
     digitalWrite(redPin, HIGH);
     Serial.println("Đèn đỏ (5 giây)");
     countdown = 5;
+    state = 1;
+  } else if(state == 1) {
+    digitalWrite(yellowPin, HIGH);
+    Serial.println("Đèn vàng (2 giây)");
+    countdown = 2;
     state = 2;
-  } else {
+  }else{
     digitalWrite(greenPin, HIGH);
     Serial.println("Đèn xanh (5 giây)");
     countdown = 5;
     state = 0;
   }
 }
-
-void loop() {
+void loop(){
   unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
+  if(currentMillis - previousMillis >= interval){
     previousMillis = currentMillis;
 
-    if (countdown < 0) {
+    if(countdown < 0){
       changeLight();
     }
 
     Serial.print("Còn ");
     Serial.print(countdown);
-    Serial.println(" giây...");
-    
+    Serial.println(" giây");
     countdown--;
+
   }
 }
