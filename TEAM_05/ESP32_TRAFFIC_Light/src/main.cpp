7c6b60531964ffec3a335570c645210eed60df9c
tuanlong23 <tuanlongnguyenkhac@gmail.com>
2025-02-21 15:55:33 +0700

    n5

--- TEAM_05/ESP32_TRAFFIC_Light/src/main.cpp
+++ TEAM_05/ESP32_TRAFFIC_Light/src/main.cpp
@@ -216,7 +216,7 @@ void CheckButtonPress()
 
     if (displayOn)
     {
-      display.setBrightness(7); // Bật màn hình
+      
       display.showNumberDec(tmCounter, true, 2, 2);
       digitalWrite(bLED, HIGH); // Đèn bật
       Serial.println("Button   => ON ");
