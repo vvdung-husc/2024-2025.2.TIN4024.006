77b26db8279bafbdcbfedbbb54475d402d00f5bd
vvhieup1 <21t1020359@husc.edu.vn>
2025-03-14 15:34:45 +0700

    hieu

--- TEAM_05/Van_Viet_Hieu/ESP32_API_Telegram/src/main.cpp
+++ TEAM_05/Van_Viet_Hieu/ESP32_API_Telegram/src/main.cpp
@@ -3,7 +3,7 @@
 /*
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/telegram-esp32-motion-detection-arduino/
-  
+
   Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 */
 
@@ -13,11 +13,11 @@
 #include <ArduinoJson.h>
 
 // Replace with your network credentials
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
+const char *ssid = "Wokwi-GUEST";
+const char *password = "";
 
 // Initialize Telegram BOT
-#define BOTtoken "7857117705:AAHJt2Cff1Hv0KGXvni1DSf4gIo4uN-AosE7857117705"  // your Bot Token (Get from Botfather)
+#define BOTtoken "7857117705:AAHJt2Cff1Hv0KGXvni1DSf4gIo4uN-AosE"  // your Bot Token (Get from Botfather)
 
 // Dùng ChatGPT để nhờ hướng dẫn tìm giá trị GROUP_ID này
 #define GROUP_ID "-4652014658" //là một số âm
@@ -28,8 +28,9 @@ UniversalTelegramBot bot(BOTtoken, client);
 const int motionSensor = 27; // PIR Motion Sensor
 bool motionDetected = false;
 
-//Định dạng chuỗi %s,%d,...
-String StringFormat(const char* fmt, ...){
+// Định dạng chuỗi %s,%d,...
+String StringFormat(const char *fmt, ...)
+{
   va_list vaArgs;
   va_start(vaArgs, fmt);
   va_list vaArgsCopy;
@@ -37,7 +38,7 @@ String StringFormat(const char* fmt, ...){
   const int iLen = vsnprintf(NULL, 0, fmt, vaArgsCopy);
   va_end(vaArgsCopy);
   int iSize = iLen + 1;
-  char* buff = (char*)malloc(iSize);
+  char *buff = (char *)malloc(iSize);
   vsnprintf(buff, iSize, fmt, vaArgs);
   va_end(vaArgs);
   String s = buff;
@@ -46,12 +47,14 @@ String StringFormat(const char* fmt, ...){
 }
 
 // Indicates when motion is detected
-void IRAM_ATTR detectsMovement() {
-  //Serial.println("MOTION DETECTED!!!");
+void IRAM_ATTR detectsMovement()
+{
+  // Serial.println("MOTION DETECTED!!!");
   motionDetected = true;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   // PIR Motion Sensor mode INPUT_PULLUP
@@ -66,28 +69,32 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(100);
   }
 
   Serial.println("");
   Serial.println("WiFi connected");
-  
+
   bot.sendMessage(GROUP_ID, "IoT Developer started up");
 }
 
-
-void loop() {
+void loop()
+{
   static uint count_ = 0;
 
-  if(motionDetected){
+  if (motionDetected)
+  {
     ++count_;
-    Serial.print(count_);Serial.println(". MOTION DETECTED => Waiting to send to Telegram");    
-    String msg = StringFormat("%u => Motion detected!",count_);
+    Serial.print(count_);
+    Serial.println(". MOTION DETECTED => Waiting to send to Telegram");
+    String msg = StringFormat("%u => Motion detected!", count_);
     bot.sendMessage(GROUP_ID, msg.c_str());
-    Serial.print(count_);Serial.println(". Sent successfully to Telegram: Motion Detected");
+    Serial.print(count_);
+    Serial.println(". Sent successfully to Telegram: Motion Detected");
     motionDetected = false;
   }
 }
